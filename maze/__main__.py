c25374ed8de67a34198dfa2edb6b7285b35a6c25
janaSunrise <warriordefenderz@gmail.com>
2021-04-06 00:27:24 +0530

    Add variables in the __main__.

---
+++ maze/__main__.py
@@ -0,0 +1,10 @@
+import pygame
+
+SIDE = 650  # The side of the GUI, Keeping it to be a square.
+GAP = 10  # The distance between 2 rows.
+WINDOW = pygame.display.set_mode((SIDE, SIDE))
+
+pygame.display.set_caption("Maze generation visualization")
+
+if __name__ == '__main__':
+    pass
